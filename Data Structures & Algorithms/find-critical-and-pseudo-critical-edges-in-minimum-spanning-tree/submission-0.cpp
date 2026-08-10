1165e889b88940abb9a8eac64beddf3fa0241f4f
Swapnil Buragohain <swapnilgohain07@gmail.com>
2026-08-10 12:43:41 +0530

    Add: find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree - submission-0

---
+++ Data Structures & Algorithms/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/submission-0.cpp
@@ -0,0 +1,118 @@
+class Solution {
+public:
+    int N;
+    class UnionFind {
+        public:
+            vector<int> parent;
+            vector<int> rank;
+
+            UnionFind(int n) {
+                parent.resize(n);
+                rank.resize(n, 0);
+                for (int i = 0; i < n; i++) {
+                    parent[i] = i;
+                }
+            }    
+        
+            int find (int x) {
+                if (x == parent[x]) 
+                    return x;
+
+                return parent[x] = find(parent[x]);
+            }
+
+            bool Union (int x, int y) {
+                int x_parent = find(x);
+                int y_parent = find(y);
+
+                if (x_parent == y_parent) 
+                    return false;
+
+                if(rank[x_parent] > rank[y_parent]) {
+                    parent[y_parent] = x_parent;
+                } else if(rank[x_parent] < rank[y_parent]) {
+                    parent[x_parent] = y_parent;
+                } else {
+                    parent[x_parent] = y_parent;
+                    rank[y_parent]++;
+                }
+                return true;
+            }
+    };
+    
+    int Kruskal(vector<vector<int>> &vec, int skip_edge, int add_edge) {
+
+        int sum = 0;
+        
+        UnionFind uf(N);
+        int edgesConnected = 0;
+        
+        if(add_edge != -1) {
+            uf.Union(vec[add_edge][0], vec[add_edge][1]);
+            sum += vec[add_edge][2];
+            edgesConnected++;
+        }
+        
+        
+        for(int i = 0; i<vec.size(); i++) {
+            
+            if(i == skip_edge)
+                continue;
+            
+            int u  = vec[i][0];
+            int v  = vec[i][1];
+            int wt = vec[i][2];
+            int parent_u = uf.find(u);
+            int parent_v = uf.find(v);
+            
+            if(parent_u != parent_v) {
+                uf.Union(u, v);
+                edgesConnected++;
+                sum += wt;
+            }
+            
+        }
+
+        if(edgesConnected != N-1)
+            return INT_MAX;
+        
+        return sum;
+    }
+    
+    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
+        N = n;
+        for(int i = 0; i<edges.size(); i++) {
+            edges[i].push_back(i);  
+        }
+        
+        auto lambda = [&](vector<int>& vec1, vector<int>& vec2) {
+            return vec1[2] < vec2[2];
+        };
+        
+        sort(begin(edges), end(edges), lambda);
+        
+        
+        int MST_WEIGHT = Kruskal(edges, -1, -1);
+
+        vector<int> critical;
+        vector<int> pseudo_critical;
+        
+        
+        
+        for(int i = 0; i<edges.size(); i++) {
+            
+            if(Kruskal(edges, i, -1) > MST_WEIGHT) { 
+                critical.push_back(edges[i][3]);
+            }
+            
+            else if(Kruskal(edges, -1, i) == MST_WEIGHT) { 
+                pseudo_critical.push_back(edges[i][3]);
+            }
+            
+        }
+        
+        
+        return {critical, pseudo_critical};
+        
+    }
+};
