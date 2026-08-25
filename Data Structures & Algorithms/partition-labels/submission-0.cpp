564eee85e7ae36dba9c2449fe0a6c4604e0f12d8
Swapnil Buragohain <swapnilgohain07@gmail.com>
2026-08-25 20:26:40 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>last(26,0);
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            last[s[i]-'a'] = i;
+        }
+        vector<int>ans;
+        int start = 0;
+        int end = 0;
+        for(int i=0;i<n;i++){
+            end = max(end, last[s[i]-'a']);
+            if(i == end){
+                ans.push_back(i-start+1);
+                start = i+1;
+            }
+        }
+        return ans;
+    }
+};
