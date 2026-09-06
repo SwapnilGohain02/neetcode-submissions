0a28682980f798bca4d7e12fafbe66b8466b56c7
Swapnil Buragohain <swapnilgohain07@gmail.com>
2026-09-06 12:53:44 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void helper(vector<int>& nums, int i, vector<int>temp){
+        if (i == nums.size()){
+            ans.push_back(temp);
+            return ;
+        }
+        
+        helper(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        helper(nums,i+1,temp);
+        temp.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        helper(nums,0,temp);
+        return ans;
+    }
+};
