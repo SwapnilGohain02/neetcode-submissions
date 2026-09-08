b02ac0b98e6bbf1d2ad41f382abadcb0eb204996
Swapnil Buragohain <swapnilgohain07@gmail.com>
2026-09-08 13:19:45 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(int n, int k , vector<int>& temp, int start){
+        if(temp.size() == k){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=start; i<=n; i++){
+            temp.push_back(i);
+            solve(n,k,temp,i+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>temp;
+        solve(n, k,temp, 1);
+        return ans;
+    }
+};
